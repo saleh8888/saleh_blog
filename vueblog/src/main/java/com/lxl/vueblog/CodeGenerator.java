de8625bbc533febd098217539c31e74263522f68
saleh8888 <50513508+saleh8888@users.noreply.github.com>
2022-04-27 22:26:28 +0800

    remove unused code

--- vueblog/src/main/java/com/lxl/vueblog/CodeGenerator.java
+++ vueblog/src/main/java/com/lxl/vueblog/CodeGenerator.java
@@ -2,31 +2,4 @@ package com.lxl.vueblog;
 
 public class CodeGenerator {
 
-
-        public static void main(String[] args) {
-//            AutoGenerator.create("jdbc:mysql://127.0.0.1:3306/myblog", "root", "Abc.123")
-//                    .globalConfig(builder -> {
-//                        builder.author("Saleh") // 设置作者
-//                                .enableSwagger() // 开启 swagger 模式
-//                                .fileOverride() // 覆盖已生成文件
-//                                .outputDir("D://mapper"); // 指定输出目录
-//                    })
-//                    .packageConfig(builder -> {
-//                        builder.parent("com.lxl.vueblog") // 设置父包名
-//                                .moduleName("system") // 设置父包模块名
-//                                .pathInfo(Collections.singletonMap(OutputFile. "D://mapper")); // 设置mapperXml生成路径
-//                    })
-//                    .strategyConfig(builder -> {
-//                        builder.addInclude("m_log") // 设置需要生成的表名
-//                                .addTablePrefix(null, null); // 设置过滤表前缀
-//                    })
-//                    .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
-//                    .execute();
-
-        }
-
-
-
-
-
 }
